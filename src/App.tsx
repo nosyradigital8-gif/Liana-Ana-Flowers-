8ccacf2d23cf981ed2938bb38029ede24068bf59
Obi Dvaid <nosyradigital8@gmail.com>
2026-01-23 07:09:14 +0100

    add

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartProvider } from "@/contexts/CartContext";
-import PromoBanner from "./components/PromoBanner";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import { Home } from "./pages/Home";
-import { Shop } from "./pages/Shop";
-import { ProductDetail } from "./pages/ProductDetail";
-import { About } from "./pages/About";
-import { Contact } from "./pages/Contact";
-import Checkout from "./pages/Checkout";
-import NotFound from "./pages/NotFound";
+import PromoBanner from "@/components/PromoBanner";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Home } from "@/pages/Home";
+import { Shop } from "@/pages/Shop";
+import { ProductDetail } from "@/pages/ProductDetail";
+import { About } from "@/pages/About";
+import { Contact } from "@/pages/Contact";
+import Checkout from "@/pages/Checkout";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PromoBanner from './components/PromoBanner';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import { Home } from './pages/Home';
-import { Shop } from './pages/Shop';
-import { ProductDetail } from './pages/ProductDetail';
-import { About } from './pages/About';
-import { Contact } from './pages/Contact';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import { Home } from '@/pages/Home';
+import { Shop } from '@/pages/Shop';
+import { ProductDetail } from '@/pages/ProductDetail';
+import { About } from '@/pages/About';
+import { Contact } from '@/pages/Contact';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen flex flex-col">
-        <PromoBanner />
         <Header />
+
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -23,6 +22,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
+
         <Footer />
       </div>
     </BrowserRouter>
